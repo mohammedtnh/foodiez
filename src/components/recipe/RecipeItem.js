35d07534d6b7655224e5f84f0e85b25c9065c4b2
Mohamed Hamdan <mohamedhamdan@Mohameds-MacBook-Pro.local>
2021-02-18 16:24:55 +0300

    recipe ingredients finally working

--- src/components/recipe/RecipeItem.js
+++ src/components/recipe/RecipeItem.js
@@ -5,18 +5,27 @@ import IngredientItem from "../ingredient/IngredientItem";
 const RecipeItem = (props) => {
   const recipe = props.recipe;
   const ingredients = props.ingredients;
+  const recipeIngredients = recipe.ingredients;
 
-  const ingredientList = ingredients.map((ingredient) => (
-    <IngredientItem key={ingredient.id} ingredient={ingredient.id} />
+  const recipeIngredientList = ingredients.filter((ingredient) => {
+    return recipeIngredients.some((_ingredient) => {
+      return ingredient.id === _ingredient.id;
+    });
+  });
+
+  const finalList = recipeIngredientList.map((ingredient) => (
+    <IngredientItem key={ingredient.id} ingredient={ingredient} />
   ));
 
+  console.log("1recipeIngredients::::", recipeIngredients);
+  console.log("2recipeIngredientList::::", recipeIngredientList);
   return (
     <ItemWrapper>
       <Link to={`/recipes/${recipe.id}`}>
         <img src={recipe.image} alt={recipe.name} />
       </Link>
       <p> {recipe.name}</p>
-      <p>{ingredientList}</p>
+      <p>{finalList}</p>
     </ItemWrapper>
   );
 };
